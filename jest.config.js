cab6b8c9564813ec5852e5b8124c118854c1da19
LULIU-9 <lu.liu4work@gmail.com>
2023-02-02 20:30:25 +1000

    delete __mock__ file

--- jest.config.js
+++ jest.config.js
@@ -8,19 +8,21 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  collectCoverage:true,
-  collectCoverageFrom:[
+
+  collectCoverage: true,
+  collectCoverageFrom: [
     'pages/*.tsx',
     '!pages/_app.tsx',
     '!pages/_document.tsx'
     ],
-  coverageThreshold:{
-    global:{
-        branches:90,
-        functions:90,
-        lines:90,
-        statements:90
+  coverageThreshold: {
+    global: {
+        branches: 90,
+        functions: 90,
+        lines: 90,
+        statements: 90
     }},
+
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/components/$1',
