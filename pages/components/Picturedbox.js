bf49bb2a8750a5bcb2f153358ef15015e9c2502d
Zihan YU <deepmemorysea@163.com>
2023-02-06 18:57:18 +1100

    init

---
+++ pages/components/Picturedbox.js
@@ -0,0 +1,11 @@
+const Picturebox = () => {
+  return (
+    <div className="big-img">
+    <img  src="img/featured-work-sec-img-1.png" alt="Dangcing Stars" />
+    <p>Dangcing Stars</p>
+    <p>2022</p>
+  </div>
+  );
+}
+
+export default Picturebox;
