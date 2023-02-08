9aed7d7edd8594fc715955469aa0be0932699174
yunshen513 <yunshen513@gmail.com>
2023-02-08 13:08:01 +1100

    fix deployment error

---
+++ components/Hero.tsx
@@ -0,0 +1,43 @@
+import styles from "@/styles/Hero.module.css";
+import { textFont, titleFont } from "@/pages/_app";
+
+const Hero = () => {
+  return (
+    <div className={styles["hero"]}>
+      <div className={styles["hero__section"]}>
+        <div>
+          <div
+            data-testid="hero__slogan"
+            className={`${titleFont.className} heading-medium ${styles["hero__slogan"]}`}
+          >
+            We are a <br />
+            digital agency
+            <br /> from Melbourne.
+          </div>
+          <div
+            data-testid="hero__txt"
+            className={`${textFont.className} text-xlarge ${styles["hero__txt"]}`}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
+          </div>
+        </div>
+        <img
+          data-testid="hero__icon"
+          className={styles["hero__icon"]}
+          src="/images/vector.png"
+          alt="hero icon"
+        />
+      </div>
+      <div className={styles["hero__section"]}>
+        <img
+          data-testid="hero__photo"
+          className={styles["hero__photo"]}
+          src="/images/hero.png"
+          alt="hero photo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
