f761908941484183cfb4468387e15540d8dbd0b3
yunshen513 <yunshen513@gmail.com>
2023-02-09 21:27:59 +1100

    test: change test description

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Home from "../pages/index";
 import "@testing-library/jest-dom";
 
-describe("Homepage renders properly", () => {
+describe("Hero section renders properly", () => {
   beforeEach(() => {
     render(<Home />);
   });
