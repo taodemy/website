a849317ba5f46970424ba08baa5e34301ab31e12
Young <yqin7516@gmail.com>
2023-02-14 14:15:04 +1100

    update

---
+++ __tests__/components/Footer/Footer.test.tsx
@@ -0,0 +1,4 @@
+import { render, screen } from "@testing-library/react";
+import Footer from "@/layouts/Footer";
+
+describe;
--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -2,25 +2,27 @@ import React from "react";
 
 type ButtonProps = {
   buttonWidth?: string;
-	buttonHeight?: string;
-	isBlack?: boolean;
-	placeHolder?: string;
+  buttonHeight?: string;
+  isBlack?: boolean;
+  placeHolder?: string;
 };
 
-const Button = ({buttonWidth, buttonHeight, isBlack, placeHolder}: ButtonProps) => {
-	const buttonBackground = isBlack ? "#000000" : "#FFFFFF";
-	const fontColor = isBlack ? "#FFFFFF" : "#000000";
-	const buttonStyle = {
-		width: buttonWidth,
-		height: buttonHeight,
-		borderRadius: "6px",
-		background: buttonBackground,
-		color: fontColor,
-	}
-	
-	return(
-		<button style={buttonStyle} className="text-meta">{placeHolder}</button>
-	)
-}
+const Button = ({ buttonWidth, buttonHeight, isBlack, placeHolder }: ButtonProps) => {
+  const buttonBackground = isBlack ? "#000000" : "#FFFFFF";
+  const fontColor = isBlack ? "#FFFFFF" : "#000000";
+  const buttonStyle = {
+    width: buttonWidth,
+    height: buttonHeight,
+    borderRadius: "6px",
+    background: buttonBackground,
+    color: fontColor,
+  };
 
-export default Button
+  return (
+    <button style={buttonStyle} className="text-meta">
+      {placeHolder}
+    </button>
+  );
+};
+
+export default Button;
