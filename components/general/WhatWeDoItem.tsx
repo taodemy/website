1350b8ef9bd15e378b5d5eddf699e6a72fbca76e
yunshen513 <yunshen513@gmail.com>
2023-02-16 21:33:15 +1100

    fix:rename custom type

--- components/general/WhatWeDoItem.tsx
+++ components/general/WhatWeDoItem.tsx
@@ -1,14 +1,14 @@
 import styles from "@/styles/WhatWeDoItem.module.css";
 import { titleFont, textFont } from "@/pages/_app";
 
-type ItemType = {
+type WhatWeDoProps = {
   title: string;
   content: string;
   icon: string;
   alt: string;
 };
 
-const WhatWeDoItem = ({ title, content, icon, alt }: ItemType) => {
+const WhatWeDoItem = ({ title, content, icon, alt }: WhatWeDoProps) => {
   return (
     <section className={styles["what-we-do-item"]}>
       <img src={icon} alt={alt}></img>
