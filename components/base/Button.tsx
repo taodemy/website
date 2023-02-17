a31aa36741af3e2c058d0a0ac9360cfd0092fa9a
Young <yqin7516@gmail.com>
2023-02-17 21:00:32 +1100

    update: add block and unblock type for Button

--- components/base/Button.tsx
+++ components/base/Button.tsx
@@ -2,24 +2,33 @@ import styles from "@/styles/Button.module.css";
 import { textFont } from "@/pages/_app";
 
 type ButtonVariant = "default" | "inverted";
+type ButtonBlockVariants = "unblocked" | "blocked";
 type ButtonProps = {
   variant?: ButtonVariant;
+  blockVariant?: ButtonBlockVariants;
 };
 
 const Button = ({
   variant = "default",
+  blockVariant = "unblocked",
   children,
   ...otherProps
 }: ButtonProps & React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  const buttonBlockVariants = {
+    unblocked: "button-unblocked",
+    blocked: "button-blocked",
+  };
+
   const buttonVariants = {
     default: "default_button",
     inverted: "inverted_button",
   };
+
   return (
     <button
-      className={`${styles.common_button} ${textFont.className} global__text-meta ${
-        styles[buttonVariants[variant]]
-      }`}
+      className={`${styles[buttonBlockVariants[blockVariant]]} ${styles.common_button} ${
+        textFont.className
+      } global__text-meta ${styles[buttonVariants[variant]]}`}
       {...otherProps}
     >
       {children}
