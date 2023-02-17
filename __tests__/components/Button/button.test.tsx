7983bd460519668ed4d8de8b44f216a9350c6c57
Young <yqin7516@gmail.com>
2023-02-17 16:49:20 +1100

    update button.test.tsx

--- __tests__/components/Button/button.test.tsx
+++ __tests__/components/Button/button.test.tsx
@@ -3,12 +3,12 @@ import Button from "@/components/base/Button";
 
 describe("Button", () => {
   beforeEach(() => {
-    render(<Button>Get in touch</Button>);
+    render(<Button>Get in touch1</Button>);
   });
 
   it("should render button in success", () => {
     const button = screen.getByRole("button", {
-      name: /get in touch/i,
+      name: /get in touch1/i,
     });
     expect(button).toHaveClass("default_button");
   });
