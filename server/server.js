508c4878ac0dd3895fe69f7aa1c1d2a06065131e
Trent1900 <lc20200212@gmail.com>
2023-02-27 22:51:34 +1100

    Feat:update server.js and .env file

---
+++ server/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middleware = jsonServer.defaults();
+
+const PORT = process.env.JSON_SERVER_PORT;
+
+server.use(middleware);
+server.use(router);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running ${PORT}`);
+});
