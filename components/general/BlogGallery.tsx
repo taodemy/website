fe0d77ef190f35071db5797459c6ad8afff90982
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-02-28 23:18:58 +1100

    feat: finish gallery section

--- components/general/BlogGallery.tsx
+++ components/general/BlogGallery.tsx
@@ -27,53 +27,63 @@ const BlogGallery = () => {
       <article className={styles["blog_gallery__small_group"]}>
         <div className={styles["blog_gallery__small_group__line_one"]}>
           <MemberCard
-            image={"/images/two_people_praying.png"}
-            title={"Things to Look for When Comparing Branding Alternatives"}
+            image={"/images/what_is_brand_strategy.png"}
+            title={"Branding: What Real Customers Have to Say"}
             subtitle={"Branding, Design"}
             direction={"column_reverse"}
             size={"sm"}
           />
           <Divider />
-          <Card
-            image={"/images/two_people_praying.png"}
-            cornerMakerLeft={"Branding, Design"}
-            title={"Things to Look for When Comparing Branding Alternatives"}
+          <MemberCard
+            image={"/images/shaping_art.png"}
+            title={"Branding: Pros and Cons They Don't Tell You"}
+            subtitle={"Branding, Design"}
+            direction={"column_reverse"}
+            size={"sm"}
           />
           <Divider />
-          <Card
-            image={"/images/channel.png"}
-            cornerMakerLeft={"Branding, Design"}
-            title={"5 Stand-out Features of Branding You Should Know"}
+          <MemberCard
+            image={"/images/hero_hand.png"}
+            title={"How to Spot the Best Branding for You: Signs and Features"}
+            subtitle={"Branding, Design"}
+            direction={"column_reverse"}
+            size={"sm"}
           />
         </div>
         <div className={styles["blog_gallery__small_group__line_two"]}>
-          <Card
-            image={"/images/two_people_praying.png"}
-            cornerMakerLeft={"Branding, Design"}
-            title={"Things to Look for When Comparing Branding Alternatives"}
+          <MemberCard
+            image={"/images/perfume.png"}
+            title={"How Much Should I Spend on Branding"}
+            subtitle={"Branding, Design"}
+            direction={"column_reverse"}
+            size={"sm"}
           />
           <Divider />
-          <Card
-            image={"/images/two_people_praying.png"}
-            cornerMakerLeft={"Branding, Design"}
-            title={"Things to Look for When Comparing Branding Alternatives"}
+          <MemberCard
+            image={"/images/hero.png"}
+            title={"Rookie Mistakes You're Making With Your Branding"}
+            subtitle={"Branding, Design"}
+            direction={"column_reverse"}
+            size={"sm"}
           />
           <Divider />
-          <Card
-            image={"/images/channel.png"}
-            cornerMakerLeft={"Branding, Design"}
-            title={"5 Stand-out Features of Branding You Should Know"}
+          <MemberCard
+            image={"/images/gabby.png"}
+            title={"Real Branding Customer Reviews You Need to See"}
+            subtitle={"Branding, Design"}
+            direction={"column_reverse"}
+            size={"sm"}
           />
-        </div>
-        <div className={styles["blog_gallery__button_group"]}>
-          <Button variant="secondary" isBlock={true}>
-            PREVIOUS
-          </Button>
-          <Button variant="secondary" isBlock={true}>
-            NEXT
-          </Button>
         </div>
       </article>
+      <div className={styles["blog_gallery__button_group"]}>
+        <Button variant="secondary" isBlock={true}>
+          PREVIOUS
+        </Button>
+        <Button variant="secondary" isBlock={true}>
+          NEXT
+        </Button>
+      </div>
     </section>
   );
 };
