728b4f4b001afe4e463a5726dabce45a7dc635d2
Kerry Yuan <jiayuu0531@outlook.com>
2023-03-06 12:47:26 +1100

    add menu icon

--- components/general/Navbar/FirstPageNavbar.tsx
+++ components/general/Navbar/FirstPageNavbar.tsx
@@ -3,7 +3,7 @@ import styles from "@/styles/Header.module.css";
 import NavbarLink from "@/components/general/Navbar/NavbarLink";
 import PageButton from "@/components/general/Navbar/PageArrow";
 import Button from "@/components/base/Button";
-import { useState } from "react";
+import { useState, useLayoutEffect } from "react";
 
 const NavBar = () => {
   const linkLists = [
@@ -37,37 +37,24 @@ const NavBar = () => {
 
   return (
     <section className={styles.header_navbar}>
-      <nav
-        className={`${showNav ? styles.header_responsive_navbar : styles.header_navbar_menu} ${
-          showNav
-            ? styles.header_responsive_navbar_fade_in
-            : styles.header_responsive_navbar_fade_out
-        }`}
-      >
+      <nav className={showNav ? styles.header_responsive_navbar : styles.header_navbar_menu}>
         {linkLists.map(({ id, linkName }) => (
           <NavbarLink key={id} linkName={linkName} link={`/${linkName.toLowerCase()}`} />
         ))}
         <PageButton />
       </nav>
-      <div className={styles.header_navbar_btns}>
-        <div className={styles.header_navbar_btn}>
-          <Button aria-label="let's_talk_button">LET&apos;S TALK</Button>
-        </div>
-        {showNav ? (
-          <img
-            src="/icons/times_icon.svg"
-            alt="close menu icon"
-            className={styles.header_navbar_close_icon}
-            onClick={showNavBar}
-          />
-        ) : (
-          <img
-            src="/icons/menu_icon.svg"
-            alt="menu icon"
-            className={styles.header_navbar_menu_icon}
-            onClick={showNavBar}
-          />
-        )}
+      <div className={styles.header_navbar_btn}>
+        <Button aria-label="let's_talk_button">LET&apos;S TALK</Button>
+      </div>
+      <div
+        className={`${styles.header_navbar_menu_icon} ${
+          showNav ? styles.header_navbar_close_icon : null
+        }`}
+        onClick={showNavBar}
+      >
+        <div className={styles.header_navbar_menu_bar1}></div>
+        <div className={styles.header_navbar_menu_bar2}></div>
+        <div className={styles.header_navbar_menu_bar3}></div>
       </div>
     </section>
   );
