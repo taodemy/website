89330b881dc8330b3b8f438f87a209140708f16e
Young <yqin7516@gmail.com>
2023-03-14 17:30:08 +1100

    update: add responsive for sendmsg section

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -15,8 +15,8 @@ export default function Contact() {
       </Head>
       <main>
         <SendMsg />
-        <OurOffices />
-        <CommonQuestions />
+        {/* <OurOffices /> */}
+        {/* <CommonQuestions /> */}
       </main>
     </>
   );
